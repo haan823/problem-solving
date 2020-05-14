6c6fbdcf841a8883c04580f4bcabe981b00789f2
sanghyun <bejimil@g.skku.edu>
2020-05-15 01:30:35 +0900

    불량 사용자

--- solution.cpp
+++ solution.cpp
@@ -1,46 +1,68 @@
 #include <string>
 #include <vector>
+#include <set>
 #include <iostream>
 
 using namespace std;
+set<int> s;
+int len;
+bool check[8];
 
-bool strcmp(string a, string b)
+void dfs(vector<string> &u_id, vector<string> &b_id, int i, int num)
 {
-    if (a.size() == b.size())
+    if (i == len)
     {
-        for (int i = 0; i < a.size(); i++)
-        {
-            if (b[i] == '*')
-            {
-                continue;
-            }
-            else if (a[i] == b[i])
-            {
-                continue;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        return true;
+        s.emplace(num);
+        return;
     }
-    else
+
+    for (int j = 0; j < u_id.size(); j++)
     {
-        return false;
+        int z;
+        if (u_id[j].size() != b_id[i].size())
+            continue;
+
+        if (check[j] == true)
+            continue;
+
+        for (z = 0; z < u_id[j].size(); z++)
+        {
+            if (b_id[i][z] == '*')
+                continue;
+            if (u_id[j][z] != b_id[i][z])
+                break;
+        }
+
+        if (z == u_id[j].size())
+        {
+            check[j] = true;
+            dfs(u_id, b_id, i + 1, num | (1 << j));
+            check[j] = false;
+        }
     }
 }
 
 int solution(vector<string> user_id, vector<string> banned_id)
 {
-    int answer = 0;
-    return answer;
+    len = banned_id.size();
+    dfs(user_id, banned_id, 0, 0);
+    return s.size();
 }
-
 int main()
 {
-    string a = "frodo";
-    string b = "fr*de";
-    bool answer = strcmp(a, b);
-    cout << answer;
+    vector<string> u, b;
+    u.push_back("frodo");
+    u.push_back("fradi");
+    u.push_back("crodo");
+    u.push_back("abc123");
+    u.push_back("frodoc");
+    b.push_back("fr*d*");
+    b.push_back("*rodo");
+    b.push_back("******");
+    b.push_back("******");
+    solution(u, b);
+    for (int i : s)
+    {
+        cout << i << " ";
+    }
 }
