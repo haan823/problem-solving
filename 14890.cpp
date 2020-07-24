4ba68245c478054781b6888c18f263ab33d5e365
sanhyun kim <haan823@naver.com>
2020-07-24 14:54:53 +0900

    14890

---
+++ 14890.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int map[100][100];
+int slide[100][100];
+int row[100];
+int column[100];
+int n, l, cnt;
+
+int main()
+{
+    cin >> n >> l;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> map[i][j];
+        }
+    }
+
+    // row search
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            if ((map[i][j] == map[i][j - 1] + 1) && (map[i][j] == map[i][j - 2] + 1))
+            {
+
+                slide[i][j - 2] = 1;
+                slide[i][j - 1] = 1;
+            }
+        }
+    }
+}
