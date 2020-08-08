8f3ae381a702404e8b7ddae298823ec26205d715
haan823 <bejimil@g.skku.edu>
2020-08-08 17:37:26 +0900

    테스트

--- test.cpp
+++ test.cpp
@@ -1,105 +1,16 @@
-#include <string>
-#include <vector>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int n;
-vector<vector<int>> network;
-vector<vector<int>> repair;
-vector<int> map;
-
-int find(vector<int> &parent, int x)
-{
-    if (x == parent[x])
-    {
-        return x;
-    }
-    else
-    {
-        return find(parent, parent[x]);
-    }
-}
-
-void union_find(vector<int> &parent, int x, int y)
-{
-    x = find(parent, x);
-    y = find(parent, y);
-
-    if (x != y)
-    {
-        if (x < y)
-        {
-            parent[y] = x;
-        }
-        else
-        {
-            parent[x] = y;
+int main(){
+    vector<int> v = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    for(int i=0; i<10; i++){
+        cout << v[i] << " " << i << "\n";
+        if(v[i] == 2){
+            v.erase(v.begin() + i);
+            i--;
         }
     }
-}
-
-bool sameparent(vector<int> &parent, int x, int y)
-{
-    x = find(parent, x);
-    y = find(parent, y);
-
-    if (x == y)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-bool is_connected(vector<vector<int>> network)
-{
-    vector<int> u;
-    for (int i = 0; i <= n; i++)
-    {
-        u.push_back(i);
-    }
-    for (int i = 0; i < network.size(); i++)
-    {
-        union_find(u, network[i][0], network[i][1]);
-    }
-    for (int i = 1; i < n; i++)
-    {
-        if (find(u, i) == find(u, i + 1))
-        {
-            continue;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    return true;
-}
-
-int solution(int n, vector<vector<int>> network, vector<vector<int>> repair)
-{
-    int answer = -2;
-
-    if (is_connected(network))
-    {
-        answer = 1;
-    }
-    else
-    {
-        answer = -1;
-    }
-
-    return answer;
-}
-
-int main()
-{
-    n = 6;
-    network = {{1, 2}, {3, 5}, {4, 2}, {5, 6}};
-    repair = {{3, 2, 10}, {5, 4, 15}};
-
-    cout << solution(n, network, repair);
+    cout << v.size();
 }
