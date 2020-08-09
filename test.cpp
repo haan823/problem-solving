d3e762e39f1a49ca52c0176dc26d4f0aefa2b4c3
김상현 <bejimil@g.skku.edu>
2020-08-10 08:44:38 +0900

    테스트

--- test.cpp
+++ test.cpp
@@ -4,13 +4,5 @@
 using namespace std;
 
 int main(){
-    vector<int> v = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    for(int i=0; i<10; i++){
-        cout << v[i] << " " << i << "\n";
-        if(v[i] == 2){
-            v.erase(v.begin() + i);
-            i--;
-        }
-    }
-    cout << v.size();
+    cout << "git";
 }
