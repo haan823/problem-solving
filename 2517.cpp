a6d274bef3cd85b461f37ca039fc96c81281a0e5
김상현 <bejimil@g.skku.edu>
2020-08-11 14:16:24 +0900

    달리기(푸는중)

---
+++ 2517.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+vector<int> v;
+
+int main(){
+    cin >> n;
+    int tmp;
+    for(int i=0; i<n; i++){
+        cin >> tmp;
+        v.push_back(tmp);
+    }
+    for (int i=v.size(); i>=0; i--){
+
+    }
+}
