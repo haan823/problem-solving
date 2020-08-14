cec87947fcfe1781f8446bb2657cc929743a7281
김상현 <bejimil@g.skku.edu>
2020-08-14 13:15:23 +0900

    이항계수2

---
+++ 11051.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int n, k;
+int dp[1001][1001];
+
+int main(){
+    cin >> n >> k;
+    for(int i=0; i<=n; i++){
+        for(int j=0; j<=i; j++){
+            if(j == 0 || i == j){
+                dp[i][j] = 1;
+            }
+            else{
+                dp[i][j] = (dp[i-1][j-1] + dp[i-1][j]) % 10007;
+            }
+        }
+    }
+    cout << dp[n][k];
+}
