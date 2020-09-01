5de0fb5b833926c9177e08a63d4d51396ff1aa3d
haan823 <bejimil@g.skku.edu>
2020-09-01 21:33:07 +0900

    경주로 건설(푸는중)

--- 경주로건설.cpp
+++ 경주로건설.cpp
@@ -1,5 +1,7 @@
+#include <iostream>
 #include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -9,42 +11,33 @@ int dy[4] = {0, 1, 0, -1};
 int n, mv = 987654321;
 vector<vector<int>> map;
 
-void dfs(int x, int y, int dir, int cost)
+void bfs(int x, int y, int dir, int cost)
 {
-    if (x == n - 1 && y == n - 1)
-    {
-        if (cost < mv)
-        {
-            mv = cost;
+    queue<vector<int>> q;
+    q.push({x, y, dir, cost});
+    while(!q.empty()){
+        int x = q.front()[0];
+        int y = q.front()[1];
+        int dir = q.front()[2];
+        int cost = q.front()[3];
+        q.pop();
+        if(x == n-1 && y == n-1){
+            mv = min(mv, cost);
         }
-        return;
-    }
-    for (int i = 0; i < 4; i++)
-    {
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        int ndir = (i % 2);
-        int ncost;
-        if (dir == -1)
-        {
-            ncost = cost + 100;
-        }
-        else
-        {
-            if (dir == ndir)
-            {
+        for(int i=0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            int ndir;
+            int ncost;
+            if(dir == -1 || dir == (i%2)){
                 ncost = cost + 100;
             }
-            else
-            {
+            else{
                 ncost = cost + 600;
             }
-        }
-        if (nx >= 0 && nx < n && ny >= 0 && ny < n && map[nx][ny] == 0 && !visit[nx][ny])
-        {
-            visit[nx][ny] = 1;
-            dfs(nx, ny, ndir, ncost);
-            visit[nx][ny] = 0;
+            if(nx>=0 && nx<n && ny>=0 && ny<n && map[nx][ny] == 0){
+
+            }
         }
     }
 }
@@ -54,8 +47,17 @@ int solution(vector<vector<int>> board)
     int answer = 0;
     n = board.size();
     map = board;
-    visit[0][0] = 1;
-    dfs(0, 0, -1, 0);
     answer = mv;
     return answer;
+}
+
+int main()
+{
+    vector<vector<int>> board = {
+        {0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 0},
+        {0, 0, 1, 0, 0},
+        {1, 0, 0, 0, 1},
+        {0, 1, 1, 0, 0}};
+    cout << solution(board);
 }
