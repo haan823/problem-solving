38594caeb5f27db898d034564ca2b636d7b55b68
haan823 <bejimil@g.skku.edu>
2020-09-04 14:47:20 +0900

    길 찾기 게임

--- 길찾기게임.cpp
+++ 길찾기게임.cpp
@@ -1,51 +1,87 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <queue>
+#include <algorithm>
 
 using namespace std;
+
+vector<int> pre, post;
 
 typedef struct _node
 {
     int x;
     int y;
-    int number;
+    int num;
+    _node * left;
+    _node * right;
 } node;
 
-struct compare
-{
-    bool operator()(const node &a, const node &b)
-    {
-        if (a.y > b.y)
-        {
-            return true;
-        }
-        else
-        {
-            return (a.x < b.x);
-        }
+bool compare(node &a, node &b){
+    if(a.y==b.y){
+        return a.x<b.x;
     }
-} bool compare(node &a, node &b)
-{
+    else{
+        return a.y>b.y;
+    }
 }
 
-node tree[10001];
+void insertnode(node * parent, node * child){
+    if(parent->x > child->x){
+        if(parent->left == NULL){
+            parent->left = child;
+        }
+        else{
+            insertnode(parent->left, child);
+        }
+    }
+    else{
+        if(parent->right == NULL){
+            parent->right = child;
+        }
+        else{
+            insertnode(parent->right, child);
+        }
+    }
+}
+
+void prefix(node * root){
+    if(root){
+        pre.push_back(root->num);
+        prefix(root->left);
+        prefix(root->right);
+    }
+}
+
+void postfix(node * root){
+    if(root){
+        postfix(root->left);
+        postfix(root->right);
+        post.push_back(root->num);
+    }
+}
 
 vector<vector<int>> solution(vector<vector<int>> nodeinfo)
 {
     vector<vector<int>> answer;
-    priority_queue<node, vector<node>, compare> pq;
-    for (int i = 0; i < nodeinfo.size(); i++)
-    {
-        pq.push({nodeinfo[i][0], nodeinfo[i][1], i + 1});
+    vector<node> nodes;
+    for(int i=0; i<nodeinfo.size(); i++){
+        nodes.push_back({nodeinfo[i][0], nodeinfo[i][1], i+1});
     }
-
+    sort(nodes.begin(), nodes.end(), compare);
+    node * root = &nodes[0];
+    for(int i=1; i<nodes.size(); i++){
+        insertnode(root, &nodes[i]);
+    }
+    prefix(root);
+    postfix(root);
+    answer.push_back(pre);
+    answer.push_back(post);
     return answer;
 }
 
 int main()
 {
-    vector<vector<int>> nodeinfo;
+    vector<vector<int>> nodeinfo = {{5, 3}, {11, 5}, {13, 3}, {3, 5}, {6, 1}, {1, 3}, {8, 6}, {7, 2}, {2, 2}};
     vector<vector<int>> answer = solution(nodeinfo);
     for (int i = 0; i < answer[0].size(); i++)
     {
