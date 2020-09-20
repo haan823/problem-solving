1b16786ba06c02a881b4c398059d9153c6f8e43d
haan823 <bejimil@g.skku.edu>
2020-09-20 16:55:04 +0900

    연구소3

--- 17142.cpp
+++ 17142.cpp
@@ -7,10 +7,55 @@ using namespace std;
 
 int n, m, empty_space, minv = 987654321;
 int map[50][50];
+int copy_map[50][50];
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, 1, 0, -1};
 vector<pair<int, int>> virus_location;
 vector<int> permu;
+
+void solve(int fill)
+{
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            copy_map[i][j] = map[i][j];
+        }
+    }
+    int time = 0;
+    queue<pair<int, int>> q;
+    for (int i = 0; i < permu.size(); i++)
+    {
+        if (permu[i])
+        {
+            q.push(virus_location[i]);
+            copy_map[virus_location[i].first][virus_location[i].second] = 3;
+        }
+    }
+    while(!q.empty()){
+        int size = q.size();
+        if(!fill){
+            minv = min(minv, time);
+            break;
+        }
+        time++;
+        for(int i=0; i<size; i++){
+            int cx = q.front().first;
+            int cy = q.front().second;
+            q.pop();
+            for(int j=0; j<4; j++){
+                int nx = cx + dx[j];
+                int ny = cy + dy[j];
+                if(nx>=0 && nx<n && ny>=0 && ny<n && (copy_map[nx][ny]==0 || copy_map[nx][ny]==2)){
+                    if(copy_map[nx][ny]==0){
+                        fill--;
+                    }
+                    copy_map[nx][ny] = 3;
+                    q.push({nx, ny});
+                }
+            }
+        }
+    }
+    return;
+}
 
 int main()
 {
@@ -41,69 +86,16 @@ int main()
 
     do
     {
-        cout << "-----------------------------------\n";
-        for(int i=0; i<permu.size(); i++){
-            if(permu[i]){
-                cout << virus_location[i].first << " " << virus_location[i].second << "\n";
-            }
-        }
-        queue<pair<int, pair<int, int>>> q;
-        int k = 0, time = 987654321;
-        int visit[50][50];
-        for (int i = 0; i < permu.size(); i++)
-        {
-            if (permu[i])
-            {
-                q.push(make_pair(0, virus_location[i]));
-                visit[virus_location[i].first][virus_location[i].second] = 1;
-            }
-        }
-        bool flag = false;
-        while (!q.empty())
-        {
-            for(int i=0; i<n; i++){
-                for(int j=0; j<n; j++){
-                    cout << map[i][j] << " ";
-                }
-                cout << "\n";
-            }
-            cout << "\n";
-            if (flag)
-            {
-                break;
-            }
-            int ct = q.front().first;
-            int cx = q.front().second.first;
-            int cy = q.front().second.second;
-            q.pop();
-            for (int i = 0; i < 4; i++)
-            {
-                int nx = cx + dx[i];
-                int ny = cy + dy[i];
-                if (nx >= 0 && nx < n && ny >= 0 && ny < n && !visit[nx][ny] && (map[nx][ny] == 0 || map[nx][ny] == 2))
-                {
-                    q.push(make_pair(ct + 1, make_pair(nx, ny)));
-                    visit[nx][ny] = 1;
-                    k++;
-                    if (k == empty_space)
-                    {
-                        flag = true;
-                        time = ct + 1;
-                        break;
-                    }
-                }
-            }
-        }
-        if (time < minv)
-        {
-            minv = time;
-        }
+        solve(empty_space);
+
     } while (next_permutation(permu.begin(), permu.end()));
 
-    if(minv == 987654321){
+    if (minv == 987654321)
+    {
         cout << -1;
     }
-    else{
+    else
+    {
         cout << minv;
     }
 }
