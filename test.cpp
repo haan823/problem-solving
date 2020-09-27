8993b8a18b217c184933176661722ceec4a1ddec
haan823 <bejimil@g.skku.edu>
2020-09-27 16:07:53 +0900

    test

--- test.cpp
+++ test.cpp
@@ -1,64 +1,90 @@
-#include <iostream>
+#include <string>
 #include <vector>
-#include <cmath>
+
 using namespace std;
 
-int map[2][45];
-int count;
-int n;
-int dx[4] = {-1, 0, 1, 0};
-int dy[4] = {0, 1, 0, -1};
+int map[15][15];
+int visit[15][15];
+int dx[6] = {-1, -1, 0, 1, 1, 0};
+int dy[6] = {-1, 0, 1, 1, 0, -1};
 
-void dfs(int i, int x, int y){
-    if(i == n-1){
-        for(int a=0; a<2; a++){
-            for(int b=0; b<n; b++){
-                cout << map[a][b] << " ";
-            }
-            cout << "\n";
+vector<int> solution(vector<vector<int>> blocks)
+{
+    vector<int> answer;
+    int total = 0;
+    for (int i = 0; i < blocks.size(); i++)
+    {
+        for (int j = i; j <= i; j++)
+        {
+            total++;
         }
-        cout << "\n";
-        count++;
-        return;
     }
-    for(int j=0; j<4; j++){
-        int cx = x + dx[j];
-        int cy = y + dy[j];
-        if(cx>=0 && cx<2 && cy>=0 && cy<n && map[x][y]==0){
-            map[x][y] = 1;
-            map[cx][cy] = 1;
-            bool flag = false;
-            int nx, ny;
-            for(int a=0; a<2; a++){
-                for(int b=0; b<n; b++){
-                    if(map[a][b] == 0){
-                        flag = true;
-                        nx = a;
-                        ny = b;
-                        break;
+    for (int i = 0; i < blocks.size(); i++)
+    {
+        map[i][blocks[i][0]] = blocks[i][1];
+        visit[i][blocks[i][0]] = 1;
+        total--;
+    }
+
+    while (total > 0)
+    {
+        for (int i = 0; i < blocks.size(); i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                for (int k = 0; k < 6; k++)
+                {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+                    if (nx >= 0 * &&nx < blocks.size() && ny >= 0 && ny <= nx && !visit[nx][ny])
+                    {
+                        if (k == 0 && visit[i][j - 1])
+                        {
+                            map[nx][ny] = map[i][j] + map[i][j - 1];
+                        }
+                        else if (k == 1 && visit[i][j + 1])
+                        {
+                            map[nx][ny] = map[i][j] + map[i][j + 1];
+                        }
+                        else if (k == 2 && visit[i - 1][j])
+                        {
+                            map[nx][ny] = map[i - 1][j] - map[i][j];
+                        }
+                        else if (k == 3 && visit[i + 1][j])
+                        {
+                            map[nx][ny] = map[i][j] - map[i + 1][j];
+                        }
+                        else if (k == 4 && visit[i + 1][j + 1])
+                        {
+                            map[nx][ny] = map[i][j] - map[i + 1][j + 1];
+                        }
+                        else if (k == 5 && visit[i - 1][j - 1])
+                        {
+                            map[nx][ny] = map[i - 1][j - 1] - map[i][j];
+                        }
+                        visit[nx][ny] = 1;
+                        total--;
                     }
                 }
-                if(flag){
-                    break;
-                }
             }
-            dfs(i+1, nx, ny);
-            map[x][y] = 0;
-            map[cx][cy] = 1;
         }
     }
-}
-
-int solution(int N) {
-    int answer = 0;
-    n = N;
-    dfs(0, 0, 0);
-    answer = count;
+    for (int i = 0; i < blocks.size(); i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            answer.push_back(map[i][j]);
+        }
+    }
     return answer;
 }
 
-int main(){
-    int N;
-    cin >> N;
-    cout << solution(N);
+int main()
+{
+    vector<vector<int>> blocks = {{0, 50}, {0, 22}, {2, 10}, {1, 4}, {4, -13}};
+    vector<int> answer = solution(blocks);
+    for (int i = 0; i < answer.size(); i++)
+    {
+        cout << answer[i] << " ";
+    }
 }
--- test3.cpp
+++
@@ -1,8 +0,0 @@
-#include <vector>
-using namespace std;
-
-int solution(vector<int> histogram) 
-{
-    int answer = 12;
-    return answer;
-}
