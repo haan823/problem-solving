869b3188ff184e278810a43ccbd22130006fe40b
SANGHYUN KIM <haan823@naver.com>
2021-11-13 20:59:21 +0900

    그리디 - 1이 될 때까지

--- python/1이 될 때까지.py
+++ python/1이 될 때까지.py
@@ -3,10 +3,14 @@ N, K = map(int, input().split())
 result = 0
 
 while True:
-    if N % K == 0:
-        result += N // K
+    if N < K:
+        result += (N - 1)
         break
+    if N % K == 0:
+        result += 1
+        N /= K
+        continue
     N -= 1
     result += 1
 
-print(result)
+print(int(result))
