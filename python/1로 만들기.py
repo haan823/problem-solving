20987cfaad43a03184ec0dbdd48252851a147f4f
SANGHYUN KIM <haan823@naver.com>
2021-11-26 22:03:04 +0900

    DP - 1로 만들기

--- python/1로 만들기.py
+++ python/1로 만들기.py
@@ -2,13 +2,13 @@ n = int(input())
 
 d = [0] * 30001
 
-for i in range(2, n + 1):
-    d[i] = d[i - 1] + 1
-    if i % 2 == 0:
-        d[i] = min(d[i], d[i // 2] + 1)
-    if i % 3 == 0:
-        d[i] = min(d[i], d[i // 3] + 1)
-    if i % 5 == 0:
-        d[i] = min(d[i], d[i // 5] + 1)
+for x in range(2, n + 1):
+    d[x] = d[x - 1] + 1
+    if x % 2 == 0:
+        d[x] = min(d[x], d[x // 2] + 1)
+    if x % 3 == 0:
+        d[x] = min(d[x], d[x // 3] + 1)
+    if x % 5 == 0:
+        d[x] = min(d[x], d[x // 5] + 1)
 
 print(d[n])
